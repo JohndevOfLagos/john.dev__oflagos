0db96d64bc130b6c47ff6691be8ebfc231217e2b
Johndevoflagos <Johndevoflagos@gmail.com>
2024-05-21 12:17:14 -0700

    Landing Home Page

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -7,14 +7,16 @@ import { Icon } from "@iconify/react";
 export const Home = () => {
   return (
     <div className="home">
-      <div className="hero"> 
+      <div className="hero">
         <div className="hero-left">
           <span className="hero-name">
-           <h5> Hi, i'm John,</h5>
-            <h3>Frontend Web and Mobile Developer</h3>
+            <h5> Hi, i'm John,</h5>
+            <h3>Frontend Developer</h3>
             <h6 className="hero-quote">
-          I'm a Frontend Web and Mobile Developer from Nigeria. In IT, change is constant. Embracing new tech, updating skills keeps me fulfilled.
-          </h6>
+              I'm a Frontend Developer from Nigeria. In IT,
+              change is constant. Embracing new tech, updating skills keeps me
+              fulfilled.
+            </h6>
           </span>
 
           <div className="hero-left-button">
@@ -26,18 +28,15 @@ export const Home = () => {
               <Icon icon="iconoir:instagram" />
             </Link>
             <Link>
-              {" "}
               <Icon icon="ri:twitter-x-fill" />
             </Link>
             <Link>
-              {" "}
               <Icon icon="mdi:linkedin" />
             </Link>
             <Link>
               <Icon icon="mdi:github" />
-            </Link> =
+            </Link>
             <Link>
-              {" "}
               <Icon icon="ic:baseline-tiktok" />
             </Link>
           </div>
@@ -55,22 +54,46 @@ export const Home = () => {
         </div>
         <div className="herobottom-body">
           <div className="herobottom-left">
-            <span>Skill</span>
-            <h4>
-              Some of the newest <span>techology</span> that i use.
-            </h4>
+            <span className="herobottom-head">Skill</span>
+            <span className="herobottom-info">
+              <span>
+                Some of the newest
+                <span>techology</span>
+                that i use.
+              </span>
+            </span>
           </div>
-          <div className="herobuttom-right">
-            <Icon icon="vscode-icons:file-type-html" />
-            <Icon icon="vscode-icons:file-type-css" />
-            <Icon icon="tabler:brand-javascript" />
-            <Icon icon="vscode-icons:file-type-js-official" />
-            <Icon icon="skill-icons:redux" />
-            <Icon icon="cib:redux" />
-            <Icon icon="vscode-icons:file-type-light-netlify" />
-            <Icon icon="vscode-icons:file-type-firebase" />
-            <Icon icon="cib:github" />
-          </div>
+   <div className="herobuttom-right">
+   <ul >
+            <li>
+              <Icon icon="vscode-icons:file-type-html" />
+            </li>
+            <li>
+              <Icon icon="vscode-icons:file-type-css" />
+            </li>
+            <li>
+              <Icon icon="tabler:brand-javascript" />
+            </li>
+            <li>
+              <Icon icon="vscode-icons:file-type-js-official" />
+            </li>
+            <li>
+              <Icon icon="skill-icons:redux" />
+            </li>
+            <li>
+              <Icon icon="cib:redux" />
+            </li>
+            <li>
+              <Icon icon="vscode-icons:file-type-light-netlify" />
+            </li>
+            <li>
+              <Icon icon="vscode-icons:file-type-firebase" />
+            </li>
+            <li>
+              <Icon icon="cib:github" />
+            </li>
+          </ul>
+   </div>
         </div>
       </div>
     </div>
